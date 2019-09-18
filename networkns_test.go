cdf7ba1abfc53598b2bd210606c94a6669cdfe42
yihuaf <fang.yihua.eric@gmail.com>
2019-09-18 12:36:47 -0700

    add thread test

--- networkns_test.go
+++ networkns_test.go
@@ -2,6 +2,7 @@ package networkns
 
 import (
 	"runtime"
+	"sync"
 	"testing"
 )
 
@@ -45,4 +46,25 @@ func TestGetNewSetDelete(t *testing.T) {
 	if !IsSame(ns, origns) {
 		t.Fatal("Reset ns failed", origns, newns, ns)
 	}
+}
+
+func TestThreaded(t *testing.T) {
+	ncpu := runtime.GOMAXPROCS(-1)
+	if ncpu < 2 {
+		t.Skip("-cpu=2 or larger required")
+	}
+
+	// Lock this thread simply to ensure other threads get used.
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+
+	wg := &sync.WaitGroup{}
+	for i := 0; i < ncpu; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			TestGetNewSetDelete(t)
+		}()
+	}
+	wg.Wait()
 }
